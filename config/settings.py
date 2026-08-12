e0d2f6d75e9eea0977e6320e6222b62534991bf2
Pushpa <pushpa01082020@gmail.com>
2026-08-12 19:54:14 +0530

    Update database settings for Render PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "backend"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "backend", "workflow-backend-2e74.onrender.com",]
 
 
 # Application definition
